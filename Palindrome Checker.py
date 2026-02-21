9255dcac4415d971cb2c86012392e51434b5fa5a
Ritesh Bhosale <riteshbhosale2071@gmail.com>
2026-02-21 18:49:52 +0530

    Add change in file

--- Palindrome Checker.py
+++ Palindrome Checker.py
@@ -1,1 +1,16 @@
 # Palindrome Checker
+import string
+
+text = input("Enter text: ")
+
+# Remove spaces, punctuation and convert to lowercase
+cleaned_text = ""
+
+for char in text:
+    if char.isalnum():  # Keeps only letters and numbers
+        cleaned_text += char.lower()
+
+if cleaned_text == cleaned_text[::-1]:
+    print("It is a palindrome!")
+else:
+    print("It is not a palindrome.")
